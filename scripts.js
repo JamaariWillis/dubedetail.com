2dc281655f6e4abb5361338fb57697da928065bd
Jamaari <jamaari1w@gmail.com>
2023-12-05 00:03:45 -0800

    new update

--- scripts.js
+++ scripts.js
@@ -5,3 +5,29 @@ menuToggle.addEventListener("click", () => {
     menu.classList.toggle("active");
     menuToggle.classList.toggle("active");
 });
+
+
+let slideIndex = 0;
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 8000); // Change slide every 2 seconds
+}
+
+// Run the slideshow when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+    showSlides();
+});
